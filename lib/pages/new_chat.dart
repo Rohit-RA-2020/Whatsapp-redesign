c9046902e2c5935cb7b4b03e1834ab13f3f6defb
Rohit ranjan <ranjanrohit812@gmail.com>
2021-03-17 16:19:39 +0530

    New chat-images

--- lib/pages/new_chat.dart
+++ lib/pages/new_chat.dart
@@ -65,43 +65,69 @@ class _NewChatState extends State<NewChat> {
             children: <Widget>[
               MenuBar(),
               Padding(padding: EdgeInsets.only(top: 20)),
-              NewChatItem(name: "Alberta Hobbs"),
+              NewChatItem(
+                name: "Alberta Hobbs",
+                imageurl:
+                    "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/3f01db52-f675-48dc-9c91-f245d99f1486/d2nqynw-af694fd2-e1ba-4e9c-badb-630a48474599.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3sicGF0aCI6IlwvZlwvM2YwMWRiNTItZjY3NS00OGRjLTljOTEtZjI0NWQ5OWYxNDg2XC9kMm5xeW53LWFmNjk0ZmQyLWUxYmEtNGU5Yy1iYWRiLTYzMGE0ODQ3NDU5OS5qcGcifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6ZmlsZS5kb3dubG9hZCJdfQ.vgj19DpSqoncvBDutgwuXP6Z-UBL5sUaZDUlz-TNUiw",
+              ),
               Divider(
                 color: Colors.green,
                 indent: 20,
                 endIndent: 20,
               ),
-              NewChatItem(name: "Aurelio Tomberlin"),
+              NewChatItem(
+                name: "Aurelio Tomberlin",
+                imageurl:
+                    "https://c8.alamy.com/comp/2E2HAN1/man-cartoon-making-love-and-peace-symbol-design-boy-male-person-people-human-and-social-media-theme-vector-illustration-2E2HAN1.jpg",
+              ),
               Divider(
                 color: Colors.green,
                 indent: 20,
                 endIndent: 20,
               ),
-              NewChatItem(name: "Corey Burgess"),
+              NewChatItem(
+                name: "Corey Burgess",
+                imageurl:
+                    "https://i.thecartoonist.me/cartoon-portrait-of-man-with-glasses.png",
+              ),
               Divider(
                 color: Colors.green,
                 indent: 20,
                 endIndent: 20,
               ),
-              NewChatItem(name: "Daniel Respass"),
+              NewChatItem(
+                name: "Daniel Respass",
+                imageurl:
+                    "https://lh3.googleusercontent.com/proxy/uY2lKiJnDvXZi2S1DucqnEnkreFfJGpofryHF4x1sh5Ep2K-ER5jNBgNjkTfXa7jGpQfnX3v2G5lkLU3ctxNUaNjtpKWi64BDjISC7lAmCg",
+              ),
               Divider(
                 color: Colors.green,
                 indent: 20,
                 endIndent: 20,
               ),
-              NewChatItem(name: "Gerard Vargas"),
+              NewChatItem(
+                name: "Gerard Vargas",
+                imageurl: "https://i.thecartoonist.me/face-cartoon-man.png",
+              ),
               Divider(
                 color: Colors.green,
                 indent: 20,
                 endIndent: 20,
               ),
-              NewChatItem(name: "Kasha Rorie"),
+              NewChatItem(
+                name: "Kasha Rorie",
+                imageurl:
+                    "https://media.wired.com/photos/5a13e21d00f49e7e1c265637/master/pass/clowes_selfportrait.jpg",
+              ),
               Divider(
                 color: Colors.green,
                 indent: 20,
                 endIndent: 20,
               ),
-              NewChatItem(name: "Jamie Patterson"),
+              NewChatItem(
+                name: "Jamie Patterson",
+                imageurl: "https://thispersondoesnotexist.com/image",
+              ),
             ],
           ),
         ]),
@@ -114,9 +140,11 @@ class NewChatItem extends StatelessWidget {
   const NewChatItem({
     Key key,
     @required this.name,
+    @required this.imageurl,
   }) : super(key: key);
 
   final String name;
+  final String imageurl;
 
   @override
   Widget build(BuildContext context) {
@@ -132,8 +160,7 @@ class NewChatItem extends StatelessWidget {
               padding: const EdgeInsets.all(8.0),
               child: CircleAvatar(
                 radius: 40,
-                backgroundImage:
-                    NetworkImage('https://thispersondoesnotexist.com/image'),
+                backgroundImage: NetworkImage(imageurl),
               ),
             ),
             Column(
